aa43d869701ea7556113855934c8aff3197cc2c4
okonow <124766370+okonow@users.noreply.github.com>
2024-06-28 17:44:51 +0300

    delete: delete empty lines from constructor in task1

--- src/main/java/org/tasks/Task1/SquareMatrixSum.java
+++ src/main/java/org/tasks/Task1/SquareMatrixSum.java
@@ -5,20 +5,13 @@ public class SquareMatrixSum {
     int[][] matrix;
 
     public SquareMatrixSum(int[][] matrix) throws NonSquareMatrixException, EmptyMatrixException {
-
         if (matrix == null || matrix.length == 0 || matrix[0] == null) {
             throw new EmptyMatrixException("Matrix is empty");
         }
         if (matrix.length != matrix[0].length) {
             throw new NonSquareMatrixException("Matrix isn't square");
         }
-
-
-            this.matrix = matrix;
-
-
-
-
+        this.matrix = matrix;
     }
 
     public int matrixSumBesideMainDiagonal() {
@@ -30,9 +23,6 @@ public class SquareMatrixSum {
                 }
             }
         }
-
         return sumNotMainDiagonal;
-
     }
-
 }
