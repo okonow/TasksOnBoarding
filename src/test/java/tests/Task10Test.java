e34847f39c0aac13ac67a6054354785103275470
okonow <124766370+okonow@users.noreply.github.com>
2024-07-04 16:44:41 +0300

    push: changes in test10task

--- src/test/java/tests/Task10Test.java
+++ src/test/java/tests/Task10Test.java
@@ -2,17 +2,27 @@ package tests;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.tasks.exceptions.WrongCountOfCubesException;
 import org.tasks.task10.ShowAllDividers;
+import org.tasks.task9.CountStairSteps;
 
 import java.util.ArrayList;
 
 public class Task10Test {
     @Test
-    void handleTest(){
+    void handleTest() {
         int number;
-        ShowAllDividers test1 = new ShowAllDividers(3,20);
+        ShowAllDividers test1 = new ShowAllDividers(3, 20);
         ArrayList<int[]> result = test1.getDividers();
         int expected = 4;
+        Assertions.assertEquals(expected, result);
+    }
+
+    @Test
+    void handleZeroTest() {
+        ShowAllDividers test1 = new ShowAllDividers(0,4);
+        ArrayList<int[]> result = test1.getDividers();
+        int expected = 0;
         Assertions.assertEquals(expected, result);
     }
 }
