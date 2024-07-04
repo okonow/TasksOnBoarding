875010ebfb13e3894783e7118bf8e61e83a538d6
okonow <124766370+okonow@users.noreply.github.com>
2024-07-04 15:34:31 +0300

    clear: task3 by extra spaces

--- src/test/java/tests/Task3Test.java
+++ src/test/java/tests/Task3Test.java
@@ -19,15 +19,13 @@ public class Task3Test {
         Assertions.assertEquals("Matrix is empty", thrown.getMessage());
     }
 
-
-
     @Test
     void testSquareSpiral() throws EmptyMatrixException, EmptyArrayException {
         int[][] matrix = createMatrix(5, 5);
         SpiralMatrixSort test1 = new SpiralMatrixSort(matrix);
         test1.sort();
-        int [][] result = test1.getMatrix();
-        int[][] expected = {{0,1,2,3,4}, {15,16,17,18,5}, {14,23,24,19,6}, {13,22,21,20,7}, {12,11,10,9,8}};
+        int[][] result = test1.getMatrix();
+        int[][] expected = {{0, 1, 2, 3, 4}, {15, 16, 17, 18, 5}, {14, 23, 24, 19, 6}, {13, 22, 21, 20, 7}, {12, 11, 10, 9, 8}};
         Assertions.assertArrayEquals(expected, result);
     }
 
@@ -42,14 +40,14 @@ public class Task3Test {
                 {4, 2, 1, 0, -2}};
         SpiralMatrixSort test1 = new SpiralMatrixSort(matrix);
         test1.sort();
-        int [][] result = test1.getMatrix();
+        int[][] result = test1.getMatrix();
         int[][] expected = {{-100, -11, -10, -9, -8},
-                            {11, 12, 12, 13, -8},
-                            {9, 56, 61, 16, -5},
-                            {8, 54, 62, 23, -4},
-                            {7, 53, 100, 42, -3},
-                            {6, 47, 46, 43, -2},
-                            {4, 2, 1, 0, -2}};
+                {11, 12, 12, 13, -8},
+                {9, 56, 61, 16, -5},
+                {8, 54, 62, 23, -4},
+                {7, 53, 100, 42, -3},
+                {6, 47, 46, 43, -2},
+                {4, 2, 1, 0, -2}};
         Assertions.assertArrayEquals(expected, result);
     }
 
