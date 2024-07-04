3561c4b51342761e9f5c21da1c354bf5e21a3cfc
okonow <124766370+okonow@users.noreply.github.com>
2024-07-04 13:13:19 +0300

    fix: task6 (find substring), its tests

--- src/main/java/org/tasks/task6/FindSubstringRewriteOthers.java
+++ src/main/java/org/tasks/task6/FindSubstringRewriteOthers.java
@@ -1,28 +1,39 @@
 package org.tasks.task6;
 
+import org.tasks.exceptions.EmptyLineException;
+
+import java.util.Objects;
+
 public class FindSubstringRewriteOthers {
     StringBuilder line;
     String subLine, selectedWord;
 
-    public FindSubstringRewriteOthers(StringBuilder line, String subLine, String selectedWord) {
+    public FindSubstringRewriteOthers(StringBuilder line, String subLine, String selectedWord) throws EmptyLineException {
+        if (line.isEmpty()) {
+            throw new EmptyLineException("Line is empty");
+
+        }
         this.line = line;
+        if (Objects.equals(subLine, "")) {
+            throw new EmptyLineException("Subline is empty");
+        }
         this.subLine = subLine;
         this.selectedWord = selectedWord;
 
     }
 
 
-    void findSubLine() {
+    public StringBuilder replace() {
 
         int lineLength = line.length();
-        int subLineLength = subLine.length() - 1;
+        int subLineLength = subLine.length();
         for (int symbolIndex = 0; symbolIndex < lineLength - subLineLength; symbolIndex++) {
             String lineSubLine = line.substring(symbolIndex, symbolIndex + subLineLength);
             if (lineSubLine.equals(subLine)) {
                 line.replace(symbolIndex, symbolIndex + subLineLength, selectedWord);
             }
         }
-
+        return line;
     }
 
 
--- src/test/java/tests/Task6Test.java
+++ src/test/java/tests/Task6Test.java
@@ -2,15 +2,34 @@ package tests;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.tasks.exceptions.EmptyLineException;
+import org.tasks.exceptions.IndexNotExistException;
 import org.tasks.task4.FindWordByIndex;
+import org.tasks.task5.FindLongestWord;
+import org.tasks.task6.FindSubstringRewriteOthers;
+
+import java.nio.charset.StandardCharsets;
 
 public class Task6Test {
 
     @Test
-    void handleTest() {
-        String line = "I love pizza, pizzing by my pizza!";
-        FindWordByIndex test1 = new FindWordByIndex(line);
-        String result = test1.find(2);
-        Assertions.assertEquals("pizza", result);
+    void handleTest() throws EmptyLineException, IndexNotExistException {
+        StringBuilder line = new StringBuilder("Big fly flies in the sky");
+
+        FindSubstringRewriteOthers test1 = new FindSubstringRewriteOthers(line, "fly", "elephant");
+        StringBuilder result = new StringBuilder(test1.replace());
+        StringBuilder expected = new StringBuilder("Big elephant flies in the sky");
+        Assertions.assertEquals(expected.toString(), result.toString());
+    }
+
+    @Test
+    void testEmptyLineException() throws EmptyLineException {
+        StringBuilder line = new StringBuilder("");
+
+        EmptyLineException thrown = Assertions.assertThrows(EmptyLineException.class, () -> {
+            FindSubstringRewriteOthers test1 = new FindSubstringRewriteOthers(line, "fly", "elephant");
+        });
+
+        Assertions.assertEquals("Line is empty", thrown.getMessage());
     }
 }
