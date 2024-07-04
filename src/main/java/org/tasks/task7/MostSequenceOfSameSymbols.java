c9dec0b1a6147bb2635c3dfc50ba6b66e1ddffed
okonow <124766370+okonow@users.noreply.github.com>
2024-07-04 13:25:50 +0300

    fix: task7 (find most sequence of symbol); add: empty_line_exception_test to task7

--- src/main/java/org/tasks/task7/MostSequenceOfSameSymbols.java
+++ src/main/java/org/tasks/task7/MostSequenceOfSameSymbols.java
@@ -1,16 +1,21 @@
 package org.tasks.task7;
 
+import org.tasks.exceptions.EmptyLineException;
+
 public class MostSequenceOfSameSymbols {
     String line;
 
-    public MostSequenceOfSameSymbols(String line) {
+    public MostSequenceOfSameSymbols(String line) throws EmptyLineException {
+        if (line.isEmpty()) {
+            throw new EmptyLineException("Line is empty");
+        }
         this.line = line;
     }
 
     public int lengthSequence() {
         int seqLength = 0, maxSeqLength = 0, lineLength = line.length();
 
-        for (int currentSymbol = 0; currentSymbol < lineLength; currentSymbol++) {
+        for (int currentSymbol = 0; currentSymbol < lineLength-1; currentSymbol++) {
 
             char cur = line.charAt(currentSymbol), next = line.charAt(currentSymbol + 1);
 
--- src/test/java/tests/Task7Test.java
+++ src/test/java/tests/Task7Test.java
@@ -2,15 +2,27 @@ package tests;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.tasks.exceptions.EmptyLineException;
+import org.tasks.task5.FindLongestWord;
 import org.tasks.task7.MostSequenceOfSameSymbols;
 
 public class Task7Test {
     @Test
-    void handleTest(){
+    void handleTest() throws EmptyLineException {
         String line = "sdkfjjdfjjjjsfdfnlsileiee";
         MostSequenceOfSameSymbols test1 = new MostSequenceOfSameSymbols(line);
         int result = test1.lengthSequence();
         int expected = 4;
         Assertions.assertEquals(expected, result);
+    }
+
+    @Test
+    void testEmptyLineException() throws EmptyLineException {
+        String line = "";
+        EmptyLineException thrown = Assertions.assertThrows(EmptyLineException.class, () -> {
+            MostSequenceOfSameSymbols test1 = new MostSequenceOfSameSymbols(line);
+        });
+
+        Assertions.assertEquals("Line is empty", thrown.getMessage());
     }
 }
